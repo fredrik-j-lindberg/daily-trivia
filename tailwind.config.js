70425f5fced7fffaf269408dcd33f8a5e9c2f9e0
Fredrik Lindberg <fredrik.j.lindberg@gmail.com>
2022-07-26 09:55:11 +0200

    Extend tailwind with some custom classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
-/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line import/no-extraneous-dependencies
+const colors = require('tailwindcss/colors');
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        0.6: '0.15rem',
+      },
+      colors: {
+        primary: colors.teal,
+        secondary: colors.gray,
+        accent: colors.amber,
+      },
+    },
   },
   plugins: [],
 };
